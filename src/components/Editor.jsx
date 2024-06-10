a2688bdf4a995b38f1ab1f989f762c1dcc33e073
thornxyz <subhrajyotighosh972@gmail.com>
2024-06-10 22:20:57 +0530

    real time editing

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,25 +1,52 @@
-import React from "react";
-import { UnControlled as CodeMirror } from "react-codemirror2";
+import { useState, useRef, useEffect } from "react";
+import { Controlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
-import 'codemirror/theme/monokai.css';
-import "codemirror/mode/clike/clike";
-import "codemirror/addon/edit/closebrackets";
-import "codemirror/addon/edit/closetag";
+import "codemirror/theme/dracula.css";
+import "codemirror/mode/javascript/javascript";
+import ACTIONS from "../../Actions";
 
-function Editor() {
-    return (
-        <div style={{ width: 'calc(100vw - 200px)' }}>
-            <CodeMirror
-                options={{
-                    lineNumbers: true,
-                    theme: 'monokai',
-                    autoCloseTags: true,
-                    autoCloseBrackets: true,
-                    mode: "text/x-csrc",
-                }}
-            />
-        </div>
-    );
-}
+const Editor = ({ socketRef, roomId }) => {
+  const [value, setValue] = useState("");
+  const editorRef = useRef();
+
+  useEffect(() => {
+    if (socketRef.current) {
+      socketRef.current.on(ACTIONS.CODE_CHANGE, ({ code }) => {
+        if (code !== null) {
+          editorRef.current.setValue(code);
+        }
+      });
+    }
+    return () => {
+      socketRef.current.off(ACTIONS.CODE_CHANGE);
+    };
+  }, [socketRef.current]);
+
+  return (
+    <div style={{ width: "calc(100vw - 200px)" }}>
+      <CodeMirror
+        value={value}
+        options={{
+          lineNumbers: true,
+          theme: "dracula",
+          mode: "javascript",
+        }}
+        onBeforeChange={(editor, data, value) => {
+          setValue(value);
+        }}
+        onChange={(editor, data, value) => {
+          const { origin } = data;
+          const code = value;
+          if (origin !== "setValue") {
+            socketRef.current.emit(ACTIONS.CODE_CHANGE, { roomId, code });
+          }
+        }}
+        editorDidMount={(editor) => {
+          editorRef.current = editor;
+        }}
+      />
+    </div>
+  );
+};
 
 export default Editor;
--- src/pages/EditorPage.jsx
+++ src/pages/EditorPage.jsx
@@ -1,48 +1,98 @@
 import { useEffect, useRef, useState } from "react";
+import toast from "react-hot-toast";
 import Client from "../components/Client";
 import Editor from "../components/Editor";
 import { initSocket } from "../socket";
-import ACTIONS from '../../Actions';
-import { useLocation } from "react-router-dom";
+import ACTIONS from "../../Actions";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 
 function EditorPage() {
   const socketRef = useRef(null);
   const location = useLocation();
+  const reactNavigator = useNavigate();
+  const { roomId } = useParams();
+
+  const [clients, setClients] = useState([]);
+
   useEffect(() => {
     const init = async () => {
       socketRef.current = await initSocket();
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed");
+        reactNavigator("/");
+      }
+
       socketRef.current.emit(ACTIONS.JOIN, {
         roomId,
         username: location.state?.username,
       });
+
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+        }
+      );
+
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
     };
+
     init();
+
+    return () => {
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+      socketRef.current.disconnect();
+    };
   }, []);
-  const [clients, setClients] = useState([{ socketId: 1, username: "Thorn" }, { socketId: 2, username: "Joe" }]);
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
   return (
     <div className="flex w-screen h-screen bg-sky-950 text-white">
-      <div className="border-r-2 border-black h-screen p-2 flex flex-col justify-between" style={{ width: '200px' }}>
+      <div
+        className="border-r-2 border-black h-screen p-2 flex flex-col justify-between"
+        style={{ width: "200px" }}
+      >
         <div>
           <h3 className="text-center font-bold text-lg">Connected</h3>
           <div className="flex gap-4 justify-center items-center flex-wrap p-4">
-            {
-              clients.map((client) => (
-                <Client key={client.socketId} username={client.username} />
-              ))
-            }
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
           </div>
         </div>
         <div className="flex flex-col">
-          <button className="bg-green-700 px-4 py-1 mt-2 text-xl font-semibold rounded-lg hover:bg-green-900" >
+          <button className="bg-green-700 px-4 py-1 mt-2 text-xl font-semibold rounded-lg hover:bg-green-900">
             Copy Room Id
           </button>
-          <button className="bg-red-700 px-4 py-1 mt-2 text-xl font-semibold rounded-lg hover:bg-red-900" >
+          <button className="bg-red-700 px-4 py-1 mt-2 text-xl font-semibold rounded-lg hover:bg-red-900">
             Leave
           </button>
         </div>
       </div>
       <div>
-        <Editor />
+        <Editor socketRef={socketRef} roomId={roomId} />
       </div>
     </div>
   );
