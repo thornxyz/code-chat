8e62a8b27b5ac144f66ebc3cc6e9349503bc44bc
thornxyz <subhrajyotighosh972@gmail.com>
2024-06-20 20:39:18 +0530

    options added

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from "react";
+
+const Dropdown = ({ options, onSelect, defaultValue }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    if (defaultValue) {
+      setSelectedOption(defaultValue);
+    }
+  }, [defaultValue]);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+    onSelect(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div
+      className="relative inline-block font-semibold text-sm"
+      style={{ width: "140px" }}
+    >
+      <div
+        className="flex justify-between items-center p-1 rounded-md bg-gray-700 cursor-pointer"
+        onClick={toggleDropdown}
+      >
+        {selectedOption || "Select option"}
+        <span
+          className={`mx-1 transform transition-transform ${
+            isOpen ? "rotate-180" : ""
+          }`}
+        >
+          ▼
+        </span>
+      </div>
+      {isOpen && (
+        <ul className="absolute rounded-md left-0 right-0 mt-1 bg-gray-700 z-10 max-h-60 overflow-y-auto">
+          {options.map((option, index) => (
+            <li
+              key={index}
+              className="p-2 hover:bg-gray-800 cursor-pointer"
+              onClick={() => handleOptionClick(option)}
+            >
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,14 +1,39 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { UnControlled as CodeMirror } from "react-codemirror2";
 import "codemirror/lib/codemirror.css";
-import "codemirror/theme/dracula.css";
+import "codemirror/theme/material-darker.css";
 import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/python/python";
 import "codemirror/addon/edit/closetag";
 import "codemirror/addon/edit/closebrackets";
 import ACTIONS from "../../Actions";
+import Dropdown from "./Dropdown";
+
+const options = ["Javascript", "C", "C++", "Python"];
 
 const Editor = ({ socketRef, roomId, onCodeChange }) => {
+  const [mode, setMode] = useState("javascript");
   const editorRef = useRef();
+
+  const handleSelect = (option) => {
+    switch (option) {
+      case "Javascript":
+        setMode("javascript");
+        break;
+      case "C++":
+        setMode("text/x-c++src");
+        break;
+      case "C":
+        setMode("text/x-csrc");
+        break;
+      case "Python":
+        setMode("text/x-python");
+        break;
+      default:
+        setMode("javascript");
+    }
+  };
 
   useEffect(() => {
     if (socketRef.current) {
@@ -25,11 +50,18 @@ const Editor = ({ socketRef, roomId, onCodeChange }) => {
 
   return (
     <div className="overflow-auto" style={{ width: "calc(100vw - 300px)" }}>
+      <div className="pl-6 px-2 py-1" style={{ background: "#212121" }}>
+        <Dropdown
+          options={options}
+          onSelect={handleSelect}
+          defaultValue={options[0]}
+        />
+      </div>
       <CodeMirror
         options={{
           lineNumbers: true,
-          theme: "dracula",
-          mode: "javascript",
+          theme: "material-darker",
+          mode: mode,
           autoCloseTags: true,
           autoCloseBrackets: true,
         }}
